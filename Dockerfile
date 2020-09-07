d9ac3f6159f0e810ea0969b93a679f8758223c6e
みなと <minato@MacBook-Air.local>
2020-09-07 20:34:03 +0900

    first commit

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM php:7-apache
+COPY app /var/www/html/
---
+++ app/index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <title>【DBC】獨協大学放送研究会</title>
+  <meta name="description" content="獨協大学放送研究会のWebサイト。新歓・活動情報を更新中！">
+  <link rel="DBC icon" href="public/icon.png">
+  <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+  <header>
+    <img class="title" src="public/title.png" alt="title icon">
+  </header>
+
+  <div class="check">
+    <p class="check-text">
+      最新情報をチェック
+    </p>
+    <a href="https://twitter.com/dokkyobc?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="false">Follow @dokkyobc</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+  </div>
+
+
+
+  <p>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+  </p>
+
+
+
+  <footer>
+    <img class="title-bw" src="public/title.png" alt="BWtitle icon">
+    <p class="copyright">©️2020 Dokkyo Broadcasting Club</p>
+  </footer>
+  <script src="js/index.js"></script>
+</body>
+</html>
