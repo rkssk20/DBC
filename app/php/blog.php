4efce946af26469a33944494384cc70f2f683d96
みなと <minato@MacBook-Air.local>
2020-11-22 15:58:50 +0900

    env

--- app/php/blog.php
+++ app/php/blog.php
@@ -1,10 +1,11 @@
 <?php
+  $aa ="mysql:host=getenv('DATABASE_HOST'); dbname=getenv('DATABSE_NAME')";
   if($_SERVER['SERVER_NAME']=='localhost'){
     // ローカル開発環境
     $pdo = new PDO('mysql:host=mysql_dbc; dbname=dbc','root','password');
   }else{
     // AWS本番環境
-    $pdo = new PDO("mysql:host=getenv('DATABASE_HOST'); dbname=getenv('DATABSE_NAME')","getenv('DATABSE_USER')","getenv('DATABASE_PASSWORD')");
+    $pdo = new PDO($aa,getenv('DATABSE_USER'),getenv('DATABASE_PASSWORD'));
   }
 
   $st = $pdo->query("SELECT * FROM post ORDER BY no DESC");
