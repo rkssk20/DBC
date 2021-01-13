a05b29c0e07e269a9000b77f1afc5c7e03aafbc6
みなと <minato@MacBook-Air.local>
2021-01-13 20:56:49 +0900

    add

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM php:7-apache
 COPY ./app /var/www/html
-RUN apt-get update
-RUN docker-php-ext-install pdo_mysql
+RUN apt-get update && \
+    docker-php-ext-install pdo_mysql
--- app/php/db.php
+++ app/php/db.php
@@ -1,17 +1,14 @@
 <?php
 
 try {
-  if($_SERVER['SERVER_NAME']=='localhost'){
-      // ローカル開発環境
-      $pdo = new PDO('mysql:host=mysql_dbc; dbname=dbc','root','password');
-    }else{
-      // AWS本番環境 
-      $HOST = getenv('DATABASE_HOST');
-      $NAME = getenv('DATABASE_NAME');
-      $USER = getenv('DATABASE_USER');
-      $PASSWORD = getenv('DATABASE_PASSWORD');
-      $pdo = new PDO("mysql:host=$HOST; dbname=$NAME","$USER","$PASSWORD");
-    }
+  
+  $HOST = getenv('MYSQL_HOST');
+  $DATABSE = getenv('MYSQL_DATABASE');
+  $USER = getenv('MYSQL_USER');
+  $PASSWORD = getenv('MYSQL_PASSWORD');
+  $pdo = new PDO("mysql:host=$HOST; dbname=$DATABSE","$USER","$PASSWORD");
+  // $pdo = new PDO("mysql:host=mysql_dbc; dbname=dbc","root","password");
+    
   return $pdo;
 
 } catch (PDOException $e) {
