e15afd4dd77a38963e6e0b00e830f4a5e0067855
みなと <minato@MacBook-Air.local>
2021-01-15 00:01:24 +0900

    add

--- app/php/db.php
+++ app/php/db.php
@@ -1,13 +1,20 @@
 <?php
 
 try {
-  
+
   $HOST = getenv('MYSQL_HOST');
   $DATABSE = getenv('MYSQL_DATABASE');
   $USER = getenv('MYSQL_USER');
   $PASSWORD = getenv('MYSQL_PASSWORD');
-  $pdo = new PDO("mysql:host=$HOST; dbname=$DATABSE","$USER","$PASSWORD");
-  // $pdo = new PDO("mysql:host=mysql_dbc; dbname=dbc","root","password");
+  
+  if($_SERVER['SERVER_NAME']=='localhost'){
+    // ローカル開発環境
+    $pdo = new PDO("mysql:host=$HOST; dbname=$DATABSE","$USER","$PASSWORD");
+  }else{
+    // AWS本番環境
+    $DATABASE_HOST = getenv('DATABASE_HOST');
+    $pdo = new PDO("mysql:host=$DATABASE_HOST; dbname=$DATABSE","$USER","$PASSWORD");
+  }
     
   return $pdo;
 
